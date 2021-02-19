a34c436bd8cc925e3c89b050fdb982d020fc63be
Vedran Kapor <vedran.kapor.sa@gmail.com>
2021-02-19 23:21:10 +0100

    heroku port

--- API/app.py
+++ API/app.py
@@ -89,4 +89,4 @@ if __name__ == '__main__':
     import mimetypes
     mimetypes.add_type('application/javascript', '.js')
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='127.0.0.1', port=port)
+    app.run(host='0.0.0.0', port=port)
