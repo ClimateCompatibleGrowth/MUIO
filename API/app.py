5f10b53a82cdc9544272a588d2aa3f48aa525a91
Vedran Kapor <vedran.kapor.sa@gmail.com>
2021-02-19 23:11:59 +0100

    heroku dunamic port

--- API/app.py
+++ API/app.py
@@ -88,4 +88,5 @@ if __name__ == '__main__':
     #Flask.__version__
     import mimetypes
     mimetypes.add_type('application/javascript', '.js')
-    app.run(host='127.0.0.1', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='127.0.0.1', port=port)
