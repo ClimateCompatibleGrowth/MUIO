d5c5199dab8ec179db3bf9c82ffeb61ba0eb1049
Vedran Kapor <vedran.kapor.sa@gmail.com>
2021-02-20 01:33:33 +0100

    deploy

--- WebAPP/Classes/Base.Class.js
+++ WebAPP/Classes/Base.Class.js
@@ -4,7 +4,7 @@ import { Html } from "./Html.Class.js";
 export class Base {
     
     static apiUrl() {
-        let apiUrl = "http://127.0.0.1:5000/";
+        let apiUrl = "https://0.0.0.0:5000/";
         return apiUrl
     }
 
