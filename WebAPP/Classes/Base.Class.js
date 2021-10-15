d086955d28882c4b67e895fd2d2c3e8991953591
VedranKapor <vedran.kapor@hotmail.com>
2021-10-15 20:13:13 +0200

    heroku deploy

--- WebAPP/Classes/Base.Class.js
+++ WebAPP/Classes/Base.Class.js
@@ -3,7 +3,7 @@ import { Html } from "./Html.Class.js";
 import { SyncS3 } from "./SyncS3.Class.js";
 
 export class Base {
-    static HEROKU = 0;
+    static HEROKU = 1;
     static AWS_SYNC = 0;
     static INIT_SYNC = 1;
 
