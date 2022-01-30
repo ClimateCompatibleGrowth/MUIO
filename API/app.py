18e53aca9eff1805bcf45f77712379084cd7c74b
VedranKapor <vedran.kapor@hotmail.com>
2022-01-30 17:46:52 +0100

    app

--- API/app.py
+++ API/app.py
@@ -109,5 +109,5 @@ if __name__ == '__main__':
         app.run(host='127.0.0.1', port=port, debug=True)
     else:
         #HEROKU
-        #app.run(host='0.0.0.0', port=port, debug=True)
-        app.run(host='127.0.0.1', port=port, debug=True)
+        app.run(host='0.0.0.0', port=port, debug=True)
+        #app.run(host='127.0.0.1', port=port, debug=True)
