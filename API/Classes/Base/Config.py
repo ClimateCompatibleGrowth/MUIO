b79b4f0d5ae1d5e67a68c70bb66068fc7bb3e747
vkapor <vedran.kapor@hotmail.com>
2023-05-22 11:02:43 +0200

    S3 bucket enviton remomed

--- API/Classes/Base/Config.py
+++ API/Classes/Base/Config.py
@@ -1,16 +1,21 @@
 from pathlib import Path
 import os
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 import platform
 
 #load environment variables
-load_dotenv()
+#load_dotenv()
 
 SYSTEM = platform.system()
 
-S3_BUCKET = os.environ.get("S3_BUCKET")
-S3_KEY = os.environ.get("S3_KEY")
-S3_SECRET = os.environ.get("S3_SECRET")
+# S3_BUCKET = os.environ.get("S3_BUCKET")
+# S3_KEY = os.environ.get("S3_KEY")
+# S3_SECRET = os.environ.get("S3_SECRET")
+
+#S3 bucket is not used in Osemosys
+S3_BUCKET = ""
+S3_KEY = ""
+S3_SECRET = ""
 
 ALLOWED_EXTENSIONS = set(['zip', 'application/zip'])
 ALLOWED_EXTENSIONS_XLS = set(['xls', 'xlsx'])
